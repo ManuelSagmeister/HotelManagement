734e1923b06593fc7d0323155da99d8af7b530d4
SanuelMagmeister <manuel.sagmeister20@gmail.com>
2025-04-04 10:08:23 +0200

    Hotel Management Setup

---
+++ queryservice/src/native-test/java/at/fhv/sem4/ExampleResourceIT.java
@@ -0,0 +1,8 @@
+package at.fhv.sem4;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+class ExampleResourceIT extends ExampleResourceTest {
+    // Execute the same tests but in packaged mode.
+}
